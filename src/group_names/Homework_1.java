12b0c7c759cea70db6ff52dfa9d8826f53443a33
kylewilliams <kylewilliams@localhost>
2024-02-02 10:21:33 -0600

    3330 Homework 1; Printing Group members names and GitHubID's.

---
+++ src/group_names/Homework_1.java
@@ -0,0 +1,13 @@
+package group_names;
+
+public class Homework_1 {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("Kyle Williams; GithubID - KTWMF6");
+		System.out.println("Kaelyn Crews; GithubID - crewskaelyn");
+		System.out.println("Logan Limbaugh; GithubID-Loganhl");
+		
+	}
+
+}
